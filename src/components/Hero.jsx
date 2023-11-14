b3afee184554e599e3867c47cf0ab7c3c8198ed6
“Oyindamola” <oyinwumi0229@gmail.com>
2023-11-14 13:22:23 +0000

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,13 +14,13 @@ const Hero = () => {
             <div className="text-white relative  mt-5">
             <h1 className="lg:text-[55px] text-[45px]  font-font2 font-bold lg:w-[700px] w-full "><span className="uppercase">Stay ahead of scams from anywhere with</span> SCAMalicious</h1>
             <p className="text-xl font-font1 lg:w-[823px] w-full ">Don’t let scammers take advantage of you! Stay one step ahead with SCAMalicious, the essential mobile application designed to help you make informed decisions and protect yourself from fraud.</p>
-            <div className="flex lg:flex-row flex-col  gap-0 items-center ">
-             <div className="flex lg:flex-row flex-col items-center gap-0">
-            <Link to="/download" className=" lg:mt-0 mt-10"> <button className="bg-yellow relative text-center text-black w-[250px] h-[56px] border-yellow rounded-lg text-[22px] font-semibold font-font1">Download App Now </button></Link>
-              <img src={Maskup} alt="" className="mt-16 w-[268px] items-center ob" />
+            <div className="flex lg:flex-row flex-col  gap-[190px] items-center ">
+             <div className="flex lg:flex-row flex-col items-center gap-0 relative">
+            <Link to="/download" className=" lg:mt-0 mt-10"> <button className="bg-[#800080] relative text-white  w-[220px] h-[54px] border-yellow rounded-lg text-[22px] font-semibold font-font1">Download App Now </button></Link>
+              <img src={Maskup} alt="" className=" w-[268px] items-center absolute -top-2 left-[200px]" />
              </div>
              <div>
-              <div className="lg:mt-[85px] mt-5 flex  lg:flex-row flex-col items-center text-[24px] gap-1"><span>5.0 App Review</span><img src={Stars} alt="" /></div>
+              <div className="  mt-20 flex  lg:flex-row flex-col items-center text-[24px] gap-1"><span>5.0 App Review</span><img src={Stars} alt="" /></div>
               <p className="text-xl">(Over 1million downloads)</p>
              </div>
           </div>
