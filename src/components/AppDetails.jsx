df631ccab4cea70927c79fad7ce983b3854b5bda
“Oyindamola” <oyinwumi0229@gmail.com>
2023-11-14 11:38:35 +0000

    updates

--- src/components/AppDetails.jsx
+++ src/components/AppDetails.jsx
@@ -10,39 +10,39 @@ const AppDetails = () => {
            <p className="text-black opacity-80 font-font1 md:text-xl text-lg font-normal mx-auto lg:w-[930px] w-full mt-5">Our cutting-edge scam detection app empowers users to swiftly identify and report fraudulent activities, offering a robust shield against online scams. With real-time alerts and user-friendly features, its your trusted ally in the fight against deception.</p>
         </div>
         <div className="w-full flex flex-wrap gap-5 mt-10 text-center ">
-          <div className=" border  rounded-[40px] p-5 lg:w-[32%] w-full shadow-lg shadow-black-500">
+          <div className=" border  rounded-[40px] p-5 lg:w-[32%] w-full shadow-lg shadow-black">
             <img src={Scam} alt="" className="items-center mx-auto " />
             <header className="text-black font-font2 font-semibold md:text-lg text-sm text-center my-3"> Easy To Use Interface For All</header>
             <p className="text-black  font-font1 font-semibold">A friendly interface that provides a clear overview of recent scans, running or completed scans, subdomains found, and other critical security metrics, enabling users to monitor their web assets efficiently. </p>
           </div>
-          <div className=" border  rounded-[40px] p-5 lg:w-[32%] w-full shadow-lg shadow-black-500">
+          <div className=" border  rounded-[40px] p-5 lg:w-[32%] w-full shadow-lg shadow-black">
             <img src={Scam} alt="" className="items-center mx-auto " />
             <header className="text-black font-font2 font-semibold md:text-lg text-sm text-center my-3">ScamQ&A</header>
             <p className="text-black  font-font1 font-semibold">A friendly interface that provides a clear overview of recent scans, running or completed scans, subdomains found, and other critical security metrics, enabling users to monitor their web assets efficiently. </p>
           </div>
-          <div className=" border  rounded-[40px] p-5 lg:w-[32%] w-full shadow-lg shadow-black-500">
+          <div className=" border  rounded-[40px] p-5 lg:w-[32%] w-full shadow-lg shadow-black">
             <img src={Scam} alt="" className="items-center mx-auto " />
             <header className="text-black font-font2 font-semibold md:text-lg text-sm text-center my-3"> Prevention Tips</header>
             <p className="text-black  font-font1 font-semibold">A friendly interface that provides a clear overview of recent scans, running or completed scans, subdomains found, and other critical security metrics, enabling users to monitor their web assets efficiently. </p>
           </div>
-          <div className=" border  rounded-[40px] p-5 lg:w-[32%] w-fullshadow-lg shadow-black-500">
+          <div className=" border  rounded-[40px] p-5 lg:w-[32%] w-full shadow-lg shadow-black">
             <img src={Scam} alt="" className="items-center mx-auto " />
             <header className="text-black font-font2 font-semibold md:text-lg text-sm text-center my-3"> Warning Signs</header>
             <p className="text-black  font-font1 font-semibold">A friendly interface that provides a clear overview of recent scans, running or completed scans, subdomains found, and other critical security metrics, enabling users to monitor their web assets efficiently. </p>
           </div>
-          <div className=" border  rounded-[40px] p-5 lg:w-[32%] w-full shadow-lg shadow-black-500">
+          <div className=" border  rounded-[40px] p-5 lg:w-[32%] w-full shadow-lg shadow-black">
             <img src={Scam} alt="" className="items-center mx-auto " />
             <header className="text-black font-font2 font-semibold md:text-lg text-sm text-center my-3"> Anti Scam Movement</header>
             <p className="text-black  font-font1 font-semibold">A friendly interface that provides a clear overview of recent scans, running or completed scans, subdomains found, and other critical security metrics, enabling users to monitor their web assets efficiently. </p>
           </div>
-          <div className=" border  rounded-[40px] p-5 lg:w-[32%] w-fullshadow-lg shadow-black-500">
+          <div className=" border  rounded-[40px] p-5 lg:w-[32%] w-full shadow-lg shadow-black">
             <img src={Scam} alt="" className="items-center mx-auto " />
             <header className="text-black font-font2 font-semibold md:text-lg text-sm text-center my-3">Report Scam</header>
             <p className="text-black  font-font1 font-semibold">A friendly interface that provides a clear overview of recent scans, running or completed scans, subdomains found, and other critical security metrics, enabling users to monitor their web assets efficiently. </p>
           </div> 
         </div>
-        <div className="w-full lg:mx-[40%] mx-[5%]">
-       <Link to='/download'>  <button className= "bg-yellow text-center shadow-lg shadow-black-500 text-black w-[224px] h-[56px] border-yellow rounded-lg text-[22px] font-semibold font-font1 items-center  mt-10">Download Now </button></Link>
+        <div className="w-full lg:mx-[40%] mx-[20%]">
+       <Link to='/download'>  <button className= "bg-yellow text-center shadow-lg shadow-black text-black w-[224px] h-[56px] border-yellow rounded-lg text-[22px] font-semibold font-font1 items-center  mt-10">Download Now </button></Link>
         </div>
     </div>
   )
--- src/components/QuickStep.jsx
+++ src/components/QuickStep.jsx
@@ -8,7 +8,7 @@ const QuickStep = () => {
        <img src={Quick} alt="" className=" absolute w-full h-full  bg-cover mix-blend-overlay"/>
        <div className="text-white text-center ">
         <h3 className="font-font2 font-bold lg:text-[50px] text-2xl pt-14">Take A Quick Step</h3>
-        <p className="font-font1 mx-auto lg:w-[856px] w-full mt-5 font-semibold md:px-0 px-8">In the age of digital risks, hesitation can cost you dearly. Downloading SCAMalicious application enables you to make quick, informed decisions, safeguarding your finances and personal information. With its intuitive interface and real-time alerts, its your first line of defense against fraudulent schemes, providing peace of mind in a fast-paced online world. </p>
+        <p className="font-font1 mx-auto lg:w-[856px] w-full mt-5 font-semibold md:px-0 px-8 lg:text-[16px] text-sm">In the age of digital risks, hesitation can cost you dearly. Downloading SCAMalicious application enables you to make quick, informed decisions, safeguarding your finances and personal information. With its intuitive interface and real-time alerts, its your first line of defense against fraudulent schemes, providing peace of mind in a fast-paced online world. </p>
         <Link to="/download" className="relative"><button className= " items-center bg-yellow text-center shadow-lg shadow-black-500 text-black lg:w-[431px] w-[250px] h-[46px] border-yellow rounded-[50px] text-[22px] font-semibold font-font1  mt-14 lg:mb-0 mb-10">Download App Now </button></Link>
        </div>
     </div>
